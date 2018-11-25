1ba57005e03d25ca8e5b4ae5e27dfb5a3eedf78a
Yufan Wang <yufansmail@gmail.com>
2018-11-24 17:40:06 -0800

    build folder

---
+++ yufanw/build/precache-manifest.fcfd02007aab1fa6838cfc7b9587adad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91a62246a9e4fd8b1a29",
+    "url": "/static/js/main.91a62246.chunk.js"
+  },
+  {
+    "revision": "dd9c8f5a550e0a1803e3",
+    "url": "/static/js/1.dd9c8f5a.chunk.js"
+  },
+  {
+    "revision": "91a62246a9e4fd8b1a29",
+    "url": "/static/css/main.6f8d0b50.chunk.css"
+  },
+  {
+    "revision": "125b02bcd9ea34f45c5a04de91eda775",
+    "url": "/index.html"
+  }
+];
