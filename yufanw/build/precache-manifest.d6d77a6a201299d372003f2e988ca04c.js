5900e619a1f9cbe5a3164ec7c47ba8c19e243217
Yufan Wang <yufanwangcn@hotmail.com>
2018-11-27 17:55:47 -0800

    toggle fix

--- yufanw/build/precache-manifest.d6d77a6a201299d372003f2e988ca04c.js
+++ yufanw/build/precache-manifest.d6d77a6a201299d372003f2e988ca04c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "8b1acaf4693622859c4f",
-    "url": "/static/js/main.8b1acaf4.chunk.js"
+    "revision": "57782bcb51e065d89c73",
+    "url": "/static/js/main.57782bcb.chunk.js"
   },
   {
     "revision": "dd9c8f5a550e0a1803e3",
     "url": "/static/js/1.dd9c8f5a.chunk.js"
   },
   {
-    "revision": "8b1acaf4693622859c4f",
-    "url": "/static/css/main.8595dfe5.chunk.css"
+    "revision": "57782bcb51e065d89c73",
+    "url": "/static/css/main.7fee0c85.chunk.css"
   },
   {
-    "revision": "ae7fb530cb6297215a7e0fa02840a8ea",
+    "revision": "583c5d8f04aad57eb3101c40f765f698",
     "url": "/index.html"
   }
 ];
--- yufanw/build/service-worker.js
+++ yufanw/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b69b1a27ebdbf6e35f92df007f1c47c6.js"
+  "/precache-manifest.d6d77a6a201299d372003f2e988ca04c.js"
 );
 
 workbox.clientsClaim();
