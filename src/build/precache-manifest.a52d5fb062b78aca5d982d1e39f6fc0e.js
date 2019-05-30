4c0a9a683d8b26e37f47589be93ba5a060860809
Yufan Wang <yufanwangcn@hotmail.com>
2019-05-30 14:10:13 -0700

    fixed security vulnerabilities

---
+++ src/build/precache-manifest.a52d5fb062b78aca5d982d1e39f6fc0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e62adedc1b930f6c31ca39633bb6924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20f17950eec006bc2bcd",
+    "url": "/static/css/main.1b9536c0.chunk.css"
+  },
+  {
+    "revision": "cfc39a008c10a992a855",
+    "url": "/static/js/2.09b4f641.chunk.js"
+  },
+  {
+    "revision": "20f17950eec006bc2bcd",
+    "url": "/static/js/main.178c307a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
