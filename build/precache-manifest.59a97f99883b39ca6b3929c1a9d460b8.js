66dcde88f16a17b7eb84e101798083455979024d
Yufan Wang <yufanwangcn@hotmail.com>
2020-09-16 01:05:23 -0700

    new build

---
+++ build/precache-manifest.59a97f99883b39ca6b3929c1a9d460b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294599f0af2056de8da37077316aa7eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9fcd832dd2fe4ef7176",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "1624aaa73bf7b72e0de5",
+    "url": "/static/js/2.0db02cfb.chunk.js"
+  },
+  {
+    "revision": "a3efdd86cef75421937f2e383c437e60",
+    "url": "/static/js/2.0db02cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fcd832dd2fe4ef7176",
+    "url": "/static/js/main.10b55ae9.chunk.js"
+  },
+  {
+    "revision": "ff022c1ff1880942318e",
+    "url": "/static/js/runtime-main.9aa4a77b.js"
+  }
+]);
