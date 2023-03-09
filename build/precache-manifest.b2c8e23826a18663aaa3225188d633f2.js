f5bb828fa89225f489f868d9e63e64df354d899c
Yufan Wang <yufanwangcn@hotmail.com>
2023-03-09 00:22:24 -0800

    new build

---
+++ build/precache-manifest.b2c8e23826a18663aaa3225188d633f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c921ccd391a03dce862120bfab3af7f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad803fdb654e3a65258c",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "73a55d3674d56d62593c",
+    "url": "/static/js/2.cd4af3a7.chunk.js"
+  },
+  {
+    "revision": "a3efdd86cef75421937f2e383c437e60",
+    "url": "/static/js/2.cd4af3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad803fdb654e3a65258c",
+    "url": "/static/js/main.3a9fd13d.chunk.js"
+  },
+  {
+    "revision": "ff022c1ff1880942318e",
+    "url": "/static/js/runtime-main.9aa4a77b.js"
+  }
+]);
