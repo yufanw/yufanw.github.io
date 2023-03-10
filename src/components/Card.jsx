3ce6ef60b745ed977fcfaa2f56246b5341341859
Yufan Wang <31011353+yufanw@users.noreply.github.com>
2023-03-10 04:50:33 -0800

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,11 @@ import {
   gray0,
   gray1,
   gray7,
+  navy,
 } from '../common/colors';
 
 const CardDiv = styled.div`
-  background: linear-gradient(to right bottom, ${blackish}, ${gray7});
+  background: linear-gradient(to right bottom, ${navy}, ${gray7});
   max-width: 980px;
   display: flex;
   flex-flow: column;
