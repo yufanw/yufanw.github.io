0c831211d64e88f4f0b01032acee3de2259644f2
Yufan Wang <31011353+yufanw@users.noreply.github.com>
2023-03-10 05:04:03 -0800

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import {
   blackish,
+  blue,
   gray0,
   gray1,
-  blue,
 } from '../common/colors';
 
 const CardDiv = styled.div`
